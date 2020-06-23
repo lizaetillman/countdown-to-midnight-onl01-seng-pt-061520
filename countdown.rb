c050a1f919fb8d8bcf445028b473dbc4f9ec3c5a
Liza Tillman <lizaetillman@gmail.com>
2020-06-23 03:24:49 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,12 +5,13 @@ def countdown(x)
     puts "#{number} SECOND(S)!"
     x -=1
     sleep 1.second
+
   end
 
 return "HAPPY NEW YEAR!"
 
 end
 
-def countdown_with_sleep
-  sleep 5.seconds
-end
+#def countdown_with_sleep
+#  sleep 5.seconds
+#end
